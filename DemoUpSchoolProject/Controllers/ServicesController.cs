df361fb78c9286a219801bdec1b49299f33ee71f
tugbakaracak <tugbakaracak98@gmail.com>
2022-10-19 17:33:13 +0300

    Add project files.

---
+++ DemoUpSchoolProject/Controllers/ServicesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        /*
+        ToList
+        Add
+        Remove
+        Where     
+         */
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+        public ActionResult Index()
+        {
+         var values= db.TblServices.ToList();
+
+
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddServices()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+            public ActionResult AddServices(TblServices p)
+        {
+            db.TblServices.Add(p);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+        }
+
+
+        public ActionResult DeleteService(int id)
+        {
+            var values = db.TblServices.Find(id);
+            db.TblServices.Remove(values);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}

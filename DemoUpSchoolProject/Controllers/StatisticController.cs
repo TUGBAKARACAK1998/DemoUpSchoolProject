5540b2cbd6c735ee9acf9b29cbeb87ad13b34131
tugbakaracak <tugbakaracak98@gmail.com>
2022-10-20 17:18:38 +0300

    second commit

---
+++ DemoUpSchoolProject/Controllers/StatisticController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+namespace DemoUpSchoolProject.Controllers
+{
+    public class StatisticController : Controller
+    {
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+
+        public ActionResult Index()
+        {
+            // Referansların Toplam Sayısı
+            ViewBag.v1 = db.TblTestimonial.Count();
+
+            // İstanbul'daki Referans Sayısı 
+            ViewBag.v2 = db.TblTestimonial.Where(x => x.City == "İstanbul").Count();
+
+            //Yazılım Mühendisi Haricindeki Kişi Sayısı
+
+            ViewBag.v3= db.TblTestimonial.Where(x => x.Profession!="Yazılım Mühendisi").Count();
+
+            //Şehri Trabzon olan Kişinin İsmi
+            ViewBag.v4 = db.TblTestimonial.Where(x => x.City == "Trabzon").Select(x => x.NameSurname).FirstOrDefault();
+
+            //Referansların Ortalama
+            ViewBag.v5 = db.TblTestimonial.Average(x => x.Balance);
+
+            return View();
+        }
+    }
+}
---
+++ DemoUpSchoolProject/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class UserController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Head()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Header()
+        {
+            return PartialView();
+        }
+        public PartialViewResult DomainModal()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SocialMedia()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Skills()
+        {
+            return PartialView();
+        }
+
+
+        public PartialViewResult FeaturedSkills()
+        {
+            return PartialView();
+        }
+
+
+        public PartialViewResult Awards()
+        {
+            return PartialView();
+        }
+
+
+
+        public PartialViewResult Testimonial()
+        {
+            return PartialView();
+        }
+
+
+        public PartialViewResult Clients()
+        {
+            return PartialView();
+        }
+
+
+        public PartialViewResult Contact()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult Footer()
+        {
+            return PartialView();
+        }
+       
+    }
+}
